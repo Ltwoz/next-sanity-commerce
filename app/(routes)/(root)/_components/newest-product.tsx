11820f0f064821eac5cdbb3bc80568f17db51e2f
Ltwoz <warrapat.102@gmail.com>
2023-11-12 23:22:37 +0700

    Fix CSS class name in newest-product component

--- app/(routes)/(root)/_components/newest-product.tsx
+++ app/(routes)/(root)/_components/newest-product.tsx
@@ -37,7 +37,7 @@ export const NewestProduct = async () => {
             </span>
           </Link>
         </div>
-        <div className="mt6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
