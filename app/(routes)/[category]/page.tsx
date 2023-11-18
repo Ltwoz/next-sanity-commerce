25c5196a1f9a522d35c56137036c7cf1b8c2a374
Ltwoz <warrapat.102@gmail.com>
2023-11-18 13:37:15 +0700

    feat: force-dynamic pages

--- app/(routes)/[category]/page.tsx
+++ app/(routes)/[category]/page.tsx
@@ -18,6 +18,8 @@ async function getData(category: string) {
   return data;
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function CategoryPage({
   params,
 }: {
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "./_components/hero";
 import { NewestProduct } from "./_components/newest-product";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lb:pb-12">
